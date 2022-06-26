573d2bee2d8330ca74c58404ccae8da20fb169c3
root <root@ip-172-26-4-152.ec2.internal>
2022-06-26 12:13:19 +0000

    2

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI, Request, HTTPException
 from geocoder import ip
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, JSONResponse
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+from nmap import PortScanner
 
+
+templates = Jinja2Templates(directory="templates")
 app=FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+sc = PortScanner()
 
 @app.get("/{port}", response_class=RedirectResponse)
 async def redirect(port: str):
-    return RedirectResponse(f"http://localhost:{port}/")
+    return RedirectResponse(f"http://54.88.107.129:{port}/")
 
-@app.middleware("http")
-async def geocode_ip(request: Request, call_next):
-    connecting_ip = request.headers['cf-connecting-ip']
-    x_forwarded_for = request.headers['x-forwarded-for']
-    if x_forwarded_for:
-        connecting_ip = x_forwarded_for
-    try:
-        location = ip(connecting_ip)
-        request.state.location = location
-    except:
-        raise HTTPException(status_code=500, detail="Unable to geocode IP")
-    response = await call_next(request)
-    return response
+@app.get('/')
+async def root(req:Request):
+    head = ip(req.headers['cf-connecting-ip']).json
+    host = head['ip']
+    res = sc.scan(host,)['nmap']['scaninfo']
+    context = {
+        **head['raw'],
+        "ports":res['tcp']['services'].split(","),
+        "request":req
+    }
+    return templates.TemplateResponse('index.html', context=context)
