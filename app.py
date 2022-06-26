fc65dc522c336e9a40d8c98e4e240f77509b20a3
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2022-06-26 08:14:13 -0500

    'a'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from starlette.staticfiles import StaticFiles
 from nmap import PortScanner
 
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="jinja2")
 app=FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 sc = PortScanner()
@@ -17,12 +17,17 @@ async def redirect(port: str):
 
 @app.get('/')
 async def root(req:Request):
-    head = ip(req.headers['cf-connecting-ip']).json
-    host = head['ip']
-    res = sc.scan(host,)['nmap']['scaninfo']
-    context = {
-        **head['raw'],
-        "ports":res['tcp']['services'].split(","),
-        "request":req
-    }
-    return templates.TemplateResponse('index.html', context=context)
+    try:
+        head = ip(req.headers['cf-connecting-ip']).json
+        host = head['ip']
+        res = sc.scan(host,)['nmap']['scaninfo']
+        context = {
+            **head['raw'],
+            "ports":res['tcp']['services'].split(","),
+            "request":req
+        }
+        return templates.TemplateResponse('index.jinja', context=context)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
